07c01500247451cbe98cd7dc376117005ac2641b
NeylaP <neyla1998@outlook.es>
2023-11-26 18:55:32 -0500

    Proyecto clean code

---
+++ src/clean-code/02-names-types.ts
@@ -0,0 +1,47 @@
+(() => {
+
+    const celsiusTemperatures = [33.6, 12.34];
+
+    const serverIp = '123.123.123.123';
+
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const usersEmails = users.map( user => user.email );
+
+    // Variables booleanas de un video juego
+    const canJump = false;
+    const canRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+    // Otros ejercicios
+   const startTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+   const endTime = new Date().getTime() - startTime;
+
+
+    // Funciones
+   function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBooksByUrl( url: string) {
+        throw new Error('Function not implemented.'+url);
+    }
+    
+    function getAreaOfSquare ( side: number ) {
+        throw new Error('Function not implemented.'+side);
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
